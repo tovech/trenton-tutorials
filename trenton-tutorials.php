753446bdb717a2915deff1ee90f9f333e174766f
Trenton Kennedy <trentontri@gmail.com>
2013-05-17 13:14:49 -0500

    changes

--- trenton-tutorials.php
+++ trenton-tutorials.php
@@ -1,2 +1,11 @@
+<?php
+/*
+Plugin Name: Trenton-tutorial
+Plugin URI: http://tovech.com
+Description: A brief description of the Plugin.
 Version: 1.0
+Author: Trenton Kennedy
+Author URI: http://tovech.com
+License: GPL2
 */
+?>
