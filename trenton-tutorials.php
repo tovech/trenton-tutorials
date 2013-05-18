c1dacaa9477ab55fb5c2907fc5ec5f7f8706ff1d
Trenton Kennedy <trentontri@gmail.com>
2013-05-18 02:12:47 -0500

    default wordpress hook to admin menu

--- trenton-tutorials.php
+++ trenton-tutorials.php
@@ -9,6 +9,14 @@ Author URI: http://tovech.com
 License: GPL2
 */
 
+add_action( 'admin_menu', 'register_my_custom_menu_page' );
 
+function register_my_custom_menu_page(){
+    add_menu_page( 'custom menu title', 'custom menu', 'manage_options', 'custompage', 'my_custom_menu_page', plugins_url( 'myplugin/images/icon.png' ), 6 ); 
+}
+
+function my_custom_menu_page(){
+    echo "Admin Page Test";	
+}
 
 ?>
